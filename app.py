cf318a588c9416ab9d00c227af747c056f50ad57
Shashkist <Shashkist@gmail.com>
2024-06-04 12:07:29 +0300

    Added mysql support

--- app.py
+++ app.py
@@ -1,54 +1,94 @@
 from flask import Flask, render_template, request, redirect
-
+import mysql.connector
 
 app = Flask(__name__)
 
+# Connect to MySQL database
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="admin",
+    database="contacts_app"
+)
+
+
 # list, set , dictionary, tuple similar to database
-contacts_list = [
-                    {
-                        'number': 1,
-                        'name': 'Arja Stark',
-                        'phone': '044545',
-                        'email': 'Valan.Margulis@Winterfel.com',
-                        'photo': 'arya-stark.jpg',
-                        'gender': 'Female'
-                    },
-                    {
-                        'number': 2,
-                        'name': 'Jon Snow',
-                        'phone': '045456465',
-                        'email': 'Night.Watch@Wall.com',
-                        'photo': 'jon-snow.jpg',
-                        'gender': 'Male'
-                    },
-                    {
-                        'number': 3,
-                        'name': 'Ned Stark',
-                        'phone': '04545',
-                        'email': 'Ned@WF.com',
-                        'photo': 'ned-stark.jpg',
-                        'gender': 'Male'
-                    },
-                    {
-                        'number': 4,
-                        'name': 'Hodor',
-                        'phone': 'hodor',
-                        'email': 'hodor@hodor.com',
-                        'photo': 'hodor.jpg',
-                        'gender': 'Male'
-                    }
-                ]
+# contacts_list = [
+#                     {
+#                         'number': 1,
+#                         'name': 'Arja Stark',
+#                         'phone': '044545',
+#                         'email': 'Valan.Margulis@Winterfel.com',
+#                         'photo': 'arya-stark.jpg',
+#                         'gender': 'Female'
+#                     },
+#                     {
+#                         'number': 2,
+#                         'name': 'Jon Snow',
+#                         'phone': '045456465',
+#                         'email': 'Night.Watch@Wall.com',
+#                         'photo': 'jon-snow.jpg',
+#                         'gender': 'Male'
+#                     },
+#                     {
+#                         'number': 3,
+#                         'name': 'Ned Stark',
+#                         'phone': '04545',
+#                         'email': 'Ned@WF.com',
+#                         'photo': 'ned-stark.jpg',
+#                         'gender': 'Male'
+#                     },
+#                     {
+#                         'number': 4,
+#                         'name': 'Hodor',
+#                         'phone': 'hodor',
+#                         'email': 'hodor@hodor.com',
+#                         'photo': 'hodor.jpg',
+#                         'gender': 'Male'
+#                     }
+#                 ]
+
+# function to find all contacts by query in the mysql database
+cursor = db.cursor(dictionary=True)
+def get_contacts():
+    cursor.execute("SELECT * FROM contacts")
+    result = cursor.fetchall()
+    return result
+
+
+# contacts_list = get_contacts()
 
 
 # function to find contact by number
 def findByNumber(number):
-    for contact in contacts_list:
-        if contact['number'] == number:
-            return contact
-    return None
+    cursor.execute("SELECT * FROM contacts WHERE number = %s", (number,))
+    result = cursor.fetchone()
+    return result
 
 
-        
+def check_contact_exist(name, email):
+    cursor.execute("SELECT * FROM contacts WHERE name = %s OR email = %s", (name, email))
+    result = cursor.fetchone()
+    return bool(result)
+
+
+def search_contacts(search_name):
+    cursor.execute("SELECT * FROM contacts WHERE name LIKE %s", ('%' + search_name + '%',))
+    result = cursor.fetchall()
+    return result
+
+
+def create_contact(name, phone, email, gender, photo):
+    cursor.execute("INSERT INTO contacts (name, phone, email, gender, photo) VALUES (%s, %s, %s, %s, %s)", (name, phone, email, gender, photo))
+    db.commit()
+    return "Contact added successfully"
+
+
+def delete_contact(number):
+    cursor.execute("DELETE FROM contacts WHERE number = %s", (number,))
+    db.commit()
+    return "Contact deleted successfully"
+
 
 @app.route('/')
 def hello():
@@ -63,7 +103,7 @@ def addContact():
 # route to view the contact list
 @app.route('/viewContacts')
 def viewContacts():
-    return render_template('index.html', contacts=contacts_list)
+    return render_template('index.html', contacts=get_contacts())
 
 
 
@@ -82,46 +122,15 @@ def createContact():
             # save the file in the server
             photo.save(file_path)
         # create a new contact
-        new_contact =  {
-                    'number': len(contacts_list) + 1,
-                    'name': fullname,
-                    'phone': phone,
-                    'email': email,
-                    'gender': gender,
-                    'photo': fullname + '.jpg'
-                }
-        # add new contact to the list (database)
-        contacts_list.append(new_contact)
+        create_contact(fullname, phone, email, gender, f'{fullname}.jpg')
     else:
         return render_template('addContactForm.html', message='Contact already exists')
     return redirect('/viewContacts')
 
 @app.route('/deleteContact/<int:number>')
 def deleteContact(number):
-    contact_to_delete = findByNumber(number)
-    if contact_to_delete:
-        contacts_list.remove(contact_to_delete)
+    delete_contact(number)
     return redirect('/viewContacts')
-
-
-def search_contacts(search_name):
-    search_results = []
-    for contact in contacts_list:
-        if search_name.lower() in contact['name'].lower():
-            search_results.append(contact)
-    return search_results
-
-
-def check_contact_exist(name, email):
-    for contact in contacts_list:
-        if (name):
-            if name.lower() == contact['name'].lower():
-                return True
-        if (email):
-            if email.lower() == contact['email'].lower():
-                return True
-    return False
-
 
 # search route to filter the contact list according to the search criteria:
 @app.route('/search', methods=['POST'])
