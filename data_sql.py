0da76e341cf9806d119a96707589fa446bcae8e3
fullstackjava082023 <141508327+fullstackjava082023@users.noreply.github.com>
2024-07-15 07:49:44 +0300

    Update data_sql.py

--- data_sql.py
+++ data_sql.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from dotenv import load_dotenv
+from migrate import create_db
 import os
 load_dotenv()
 
@@ -8,13 +9,37 @@ db = mysql.connector.connect(
     host=os.getenv("DB_HOST", "localhost"),
     user=os.getenv("DB_USER", "root"),
     password=os.getenv("DB_PASSWORD", "admin"),
-    database=os.getenv("DB_NAME", "contacts_app"),
-    port = os.getenv("DB_PORT", 3306)
+    # database=os.getenv("DB_NAME", "contacts_app"),
+    port=os.getenv("DB_PORT", 3306)
 )
+
+def create_db():
+    db_name = os.getenv('DB_NAME', 'contacts_app')
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+    cursor.execute(f"USE {db_name}")
+    db.commit()
+    print(f"Database {db_name} created successfully")
+
+
+def create_contacts_table():
+    cursor.execute("CREATE TABLE IF NOT EXISTS contacts ("
+                   "number INT AUTO_INCREMENT PRIMARY KEY,"
+                   "name VARCHAR(255) NOT NULL,"
+                   "phone VARCHAR(255),"
+                   "email VARCHAR(255) NOT NULL,"
+                   "gender VARCHAR(10),"
+                   "photo  VARCHAR(255))")
+    db.commit()
+    print("Table created successfully")
+
 
 
 # function to find all contacts by query in the mysql database
 cursor = db.cursor(dictionary=True)
+create_db()
+create_contacts_table()
+
+
 def get_contacts():
     cursor.execute("SELECT * FROM contacts")
     result = cursor.fetchall()
